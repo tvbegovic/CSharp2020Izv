a3d92ab856ccbad3ada7ba34bcef89641cdc1d09
Tvrtko Begović <tvbegovic@adamant69.onmicrosoft.com>
2020-12-02 18:46:51 +0100

    Klase

---
+++ Igra_Klase/Glavna.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Igra_Klase
+{
+	public partial class Glavna : Form
+	{
+		public Glavna()
+		{
+			InitializeComponent();
+		}
+
+		private void btnUnesi_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var naziv = txtNaziv.Text;
+				var opis = txtOpis.Text;
+				var vrsta = txtVrsta.Text;
+				var izdavac = txtIzdavac.Text;
+				var ok = decimal.TryParse(txtCijena.Text, out decimal cijena);
+				if (!ok)
+				{
+					MessageBox.Show("Pogrešan format");
+					return;
+				}
+				ok = DateTime.TryParse(txtDatumIzdavanja.Text, out DateTime datumIzdavanja);
+				if (!ok)
+				{
+					MessageBox.Show("Pogrešan format");
+					return;
+				}
+				var igra = new Igra();
+				igra.Naziv = naziv;
+				igra.Opis = opis;
+				igra.Izdavac = izdavac;
+				igra.Vrsta = vrsta;
+				igra.Cijena = cijena;
+				igra.DatumIzdavanja = datumIzdavanja;
+				MessageBox.Show("Podaci su uspješno uneseni");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Dogodila se pogreška. Tekst: {ex.Message}");
+			}
+		}
+	}
+}
---
+++ Igra_Klase/Igra.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Igra_Klase
+{
+	public class Igra
+	{
+		private string naziv, opis, izdavac, vrsta;
+		private decimal cijena;
+		private DateTime datumIzdavanja;
+
+		public string Naziv
+		{
+			get => naziv;
+			set 
+			{ 
+				if(string.IsNullOrEmpty(value))
+				{
+					throw new ArgumentException("Naziv ne smije biti prazan");
+				}
+				naziv = value; 
+			}
+		}
+		public string Opis { get => opis; set => opis = value; }
+		public string Izdavac { get => izdavac; set => izdavac = value; }
+		public string Vrsta { get => vrsta; set => vrsta = value; }
+		public decimal Cijena
+		{
+			get => cijena;
+			set
+			{
+				if(value < 0)
+				{
+					throw new ArgumentException("Cijena ne smije biti negativna");
+				}
+				cijena = value;
+			}
+		}
+		public DateTime DatumIzdavanja
+		{
+			get => datumIzdavanja;
+			set
+			{
+				if(value > DateTime.Now)
+				{
+					throw new ArgumentException("Datum ne smije biti u budućnosti");
+				}
+				datumIzdavanja = value;
+			}
+		}
+	}
+}
